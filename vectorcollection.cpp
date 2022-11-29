9539932ab8401d0bfff1f8e0f90b2a76660a50b3
wcallaghan <90016258+wcallaghan@users.noreply.github.com>
2022-11-29 10:39:36 -0500

    Add files via upload

---
+++ vectorcollection.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+
+using std::cin; using std::cout; using std::endl; using std::vector;
+
+int main() {
+	char operation;
+	double number;
+	vector<double> collection;
+
+	do {
+		cout << "enter operation [a/r/q] and number:" << endl;
+		cin >> operation;
+		cin >> number;
+
+		switch (operation) {
+		case 'a':
+			vector<double>::iterator it;
+
+		case 'r':
+		case 'q':
+		}
+	} while (0);
+}
